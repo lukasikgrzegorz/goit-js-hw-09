7ac820e9bf2e0ab7f6e8158263ed64259dba7c0d
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-10-24 23:49:22 +0200

    task1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,25 @@
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let mainInterval;
+
+btnStart = document.querySelector('[data-start]');
+btnStop = document.querySelector('[data-stop]');
+
+btnStop.setAttribute("disabled", "");
+
+btnStart.addEventListener("click", () => {
+    mainInterval = setInterval(() => document.body.style.backgroundColor = getRandomHexColor(), 1000);
+    btnStart.setAttribute("disabled", "");
+    btnStop.removeAttribute("disabled", "");
+})
+
+btnStop.addEventListener("click", () => {
+    clearInterval(mainInterval);
+    btnStart.removeAttribute("disabled", "");
+    btnStop.setAttribute("disabled", "");
+})
+
+
+
