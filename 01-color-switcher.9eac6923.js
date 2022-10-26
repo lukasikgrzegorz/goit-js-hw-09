69ab1301f3008b82af59467c436a555bfe74df0e
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-10-26 17:07:35 +0000

    Deploying to gh-pages from @ lukasikgrzegorz/goit-js-hw-09@e554d5719115ba25b47b9a20cbad381338fd94fb 🚀

---
+++ 01-color-switcher.9eac6923.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");r.setAttribute("disabled",""),e.addEventListener("click",(function(){t=setInterval((function(){return document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.setAttribute("disabled",""),r.removeAttribute("disabled")})),r.addEventListener("click",(function(){clearInterval(t),e.removeAttribute("disabled"),r.setAttribute("disabled","")}))}();
+//# sourceMappingURL=01-color-switcher.9eac6923.js.map
